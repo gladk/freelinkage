e0951d0e05ef5a4d9c6412e8da43c72b3dd7f50a
Anton Gladky <gladky.anton@gmail.com>
2013-11-13 21:42:31 +0100

    Fix read/write in json format.

--- bin/writejson.cpp
+++ bin/writejson.cpp
@@ -23,6 +23,7 @@
 #include <boost/property_tree/json_parser.hpp>
 
 #include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
 #include <Eigen/Core>
 
 #include <vector>
@@ -39,10 +40,15 @@ void writejson() {
   nodes.push_back(Eigen::Vector3d(0,0,0));
   nodes.push_back(Eigen::Vector3d(1110,0,0));
   nodes.push_back(Eigen::Vector3d(555,2180,0));
+  
+  unsigned int i = 0;
+  pt.put("nodes_number", nodes.size());
+  
   BOOST_FOREACH(const Eigen::Vector3d &node, nodes) {
-      pt.add("node", node(0));
-      pt.add("node", node(1));
-      pt.add("node", node(2));
+      pt.add("nodes.node_" + boost::lexical_cast<std::string>(i) + ".x", node(0));
+      pt.add("nodes.node_" + boost::lexical_cast<std::string>(i) + ".y", node(1));
+      pt.add("nodes.node_" + boost::lexical_cast<std::string>(i) + ".z", node(2));
+      i++;
   }
   
   write_json ("example.json", pt);
@@ -50,9 +56,22 @@ void writejson() {
   ptree pt_read;
 
   read_json("example.json", pt_read);
-  BOOST_FOREACH(ptree::value_type &v, pt.get_child("node")) {
-    //nodes_read.push_back(static_cast <Eigen::Vector3d> v.second.data());
-    std::cerr<<v.second.data()<<std::endl;
+  
+  
+  int nodes_number = pt.get<int>("nodes_number", -1);
+  
+  if (nodes_number>0) {
+    for (unsigned i=0; i < nodes_number; i++){
+      nodes_read.push_back(Eigen::Vector3d(
+        pt.get<double>("nodes.node_"+boost::lexical_cast<std::string>(i)+".x"), 
+        pt.get<double>("nodes.node_"+boost::lexical_cast<std::string>(i)+".y"), 
+        pt.get<double>("nodes.node_"+boost::lexical_cast<std::string>(i)+".z")));
+    }
+  }
+  
+  
+  BOOST_FOREACH(Eigen::Vector3d i, nodes_read) {
+    std::cerr<<i(0)<< " " << i(1) << " " << i(2) <<std::endl;
   }
 }
 
